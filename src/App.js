b0696339962dbc4e738dbed5e05c7607eddb931f
TranDinhDat <dattdhe186026@fpt.edu.vn>
2025-07-02 11:53:18 +0700

    Cập nhật App.js và SearchPosts.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 import Login from "./components/Login";
-import SignUp from "./components/Signup";
+import Signup from "./components/Signup"; // Sửa thành Signup
 import welcome from "./components/welcome";
 import forgotpassword from "./components/forgotpassword";
-
-
+import SearchPosts from "./components/SearchPosts";
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route exact path="/" component={SignUp} />
+        <Route exact path="/" component={welcome} />
         <Route path="/signin" component={Login} />
-        <Route path="/signup" component={SignUp} />
+        <Route path="/signup" component={Signup} />
         <Route path="/welcome" component={welcome} />
         <Route path="/forgotpassword" component={forgotpassword} />
+        <Route path="/search-posts" component={SearchPosts} />
       </Switch>
     </Router>
   );
